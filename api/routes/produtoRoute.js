b066afecb6709c610637afa07cbf89169299d7e0
alanserafim <alan.serafim@fatec.sp.gov.br>
2024-10-18 08:47:48 -0300

    aula 05: parte 1

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,13 +1,17 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
+const roles = require('../middlewares/roles')
+const permissoes = require("../middlewares/permissoes")
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', ProdutoController.buscarTodosProdutos)
+  .get('/produto', permissoes(['deletar']), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
 
 module.exports = router
+
+//.get('/produto', roles(['Gerente']), ProdutoController.buscarTodosProdutos)
